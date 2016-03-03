4c6a13a94fffcb8bed5624fbae3f35e7700978fc
gjmh-unikent <g.haddow@kent.ac.uk>
2016-03-03 15:05:21 +0000

    prevent underscore from being loaded

--- config/browserify.js
+++ config/browserify.js
@@ -3,7 +3,7 @@ var hbsfy = require('hbsfy');
 module.exports = {
     dev: {
         options: {
-            ignore: ['jquery'],
+            ignore: ['jquery', 'underscore'],
             transform: [
                 hbsfy,
                 ['aliasify', {
@@ -24,7 +24,7 @@ module.exports = {
     },
     test: {
         options: {
-            ignore: ['jquery'],
+            ignore: ['jquery', 'underscore'],
             transform: [
                 hbsfy,
                 ['aliasify', {
@@ -45,7 +45,7 @@ module.exports = {
     },
     deploy: {
         options: {
-            ignore: ['jquery'],
+            ignore: ['jquery', 'underscore'],
             transform: [
                 hbsfy,
                 ['aliasify', {
