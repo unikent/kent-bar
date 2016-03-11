17eb7ea010f04d5938fe54a2dc3df2da2c86dc8d
gjmh-unikent <g.haddow@kent.ac.uk>
2016-03-11 08:48:48 +0000

    fix linting

--- src/js/collections/base.js
+++ src/js/collections/base.js
@@ -10,29 +10,28 @@ module.exports = Backbone.Collection.extend({
 	did_fetch:false,
 
 	storeResponse: function(resp){
-		if(this.did_fetch) {
-			localStorage.setItem(this.cache_key, JSON.stringify({timestamp: Date.now(),payload:resp}));
+		if (this.did_fetch) {
+			localStorage.setItem(this.cache_key, JSON.stringify({timestamp: Date.now(), payload:resp}));
 		}
 	},
 
 	fetch: function (options) {
 		var success = options.success;
 		var that = this;
-        var rawResponse= false;
 
 
-		if(this.cache_key && localStorage && localStorage.getItem(this.cache_key)){
+		if (this.cache_key && localStorage && localStorage.getItem(this.cache_key)){
 
 			var resp = JSON.parse(localStorage.getItem(this.cache_key));
 
-			if(resp.timestamp > (Date.now() - 86400000)) {
+			if (resp.timestamp > (Date.now() - 86400000)) {
 				resp = resp.payload;
 				options.success = function (resp) {
-					if (success) success(that, resp, options);
+					if (success){ success(that, resp, options); }
 				};
-				var method = options.reset ? 'reset' : 'set';
+				var method = options.reset ? "reset" : "set";
 				this[method](this.parse(resp), options);
-				this.trigger('sync', this, resp, options);
+				this.trigger("sync", this, resp, options);
 				return resp;
 			}
 		}
