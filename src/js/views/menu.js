04396cf36f828b72083a2c42a97eaf1d7eac6756
thybag <thybag@gmail.com>
2016-03-11 17:04:45 +0000

    in progress

--- src/js/views/menu.js
+++ src/js/views/menu.js
@@ -1,5 +1,6 @@
 var BaseView = require("./base"),
 	helper = require("../lib/helper"),
+	quickspot = require("quick-spot"),
 	template = require("../templates/menu.hbs");
 
 module.exports = BaseView.extend({
@@ -19,6 +20,9 @@ module.exports = BaseView.extend({
 			"favroites": this.el.querySelector(".favorites-section")
 		};
 
+		// Boot quickspot instance
+		this.qs.instance = quickspot.attach({target: this.el.querySelector("#kent-bar-search"), data: {} });
+
 		// Close on click off
 		var that = this;
 		document.body.addEventListener("click", function(e){
@@ -32,6 +36,7 @@ module.exports = BaseView.extend({
 	isOpen: false,
 	services: false,
 	sections: {},
+	qs: { datastores: {}},
 	open: function(menu){
 		// handle request to open a given menu
 		if (this.currentMenu === menu && this.isOpen){
@@ -67,7 +72,35 @@ module.exports = BaseView.extend({
 			}
 
 			here.renderKeyServices(user_services);
+
+			here.renderServicesSearch(menu);
 		});
+	},
+	renderServicesSearch: function(type){
+		// set placeholder
+		this.qs.instance.target.placeholder = "Search " + type + " systems and services...";
+
+		window.quickspot = quickspot;
+		window.dataz = this.services.filterWithTags(['general', type]);
+
+
+		if(typeof this.qs.datastores[type] === 'undefined'){
+			
+			var payload = this.services.filterWithTags(['general', type]);
+
+			console.log(this._quickspoify(payload));
+			//console.log(quickspot.datastore({data: payload, search_on: [] }));
+			//console.log("!!!!!!!");
+
+			//console.log(this.qs.datastores[type]);
+			window.ds = this.qs.datastores[type];
+		}
+
+		this.qs.instance.datastore = this.qs.datastores[type];
+		this.qs.instance.lastValue = '';
+		
+		
+		
 	},
 	renderKeyServices: function(services){
 		var markup = "";
@@ -75,5 +108,18 @@ module.exports = BaseView.extend({
 			markup += "<a href=\"" + service.get("url") + "\" class=\"key-service " + service.get("icon") + "\">" + service.get("title") + "</a>";
 		});
 		this.sections.keyServices.innerHTML = markup;
+	},
+	_quickspoify: function(collection){
+		var data_set = [], tmp;
+
+		collection.forEach(function(item){
+			tmp = item.attributes;
+			tmp.model = item;
+			tmp.tags = tmp.tags.join(' ');
+			data_set.push(tmp)
+		});
+		console.log(data_set);
+
+		return quickspot.datastore({data: data_set, search_on: ['id', 'title', 'tags'] , 'key_value': 'title'});
 	}
 });
